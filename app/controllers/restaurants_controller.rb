152e505d29fa650cb5cdc09b9df19705b1ed7cd6
loganjacob76 <nagolisback@gmail.com>
2021-07-14 17:28:22 -0600

    Update restaurants controller

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -2,6 +2,7 @@ class RestaurantsController < ApplicationController
   def index
     zip = params[:zip]
     restaurants = RestaurantFacade.open_restaurants(zip)
+
     render json: RestaurantSerializer.new(restaurants)
   end
 end
