9f266fe4bdd406e696009653de19196cf7469ef1
loganjacob76 <nagolisback@gmail.com>
2021-07-19 16:21:03 -0600

    Add default value to votes

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_19_212115) do
+ActiveRecord::Schema.define(version: 2021_07_19_215243) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,7 +25,7 @@ ActiveRecord::Schema.define(version: 2021_07_19_212115) do
   create_table "restaurants", force: :cascade do |t|
     t.bigint "event_id"
     t.string "yelp_id"
-    t.integer "votes"
+    t.integer "votes", default: 0
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["event_id"], name: "index_restaurants_on_event_id"
