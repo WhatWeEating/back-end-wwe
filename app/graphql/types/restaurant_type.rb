bc789fc46873f21eb29bdeac6891902d3ee032d6
loganjacob76 <nagolisback@gmail.com>
2021-07-19 16:38:56 -0600

    Update restaurant type for all data

--- app/graphql/types/restaurant_type.rb
+++ app/graphql/types/restaurant_type.rb
@@ -4,6 +4,9 @@ module Types
     field :event, Types::EventType, null: false
     field :yelp_id, String, null: true
     field :votes, Integer, null: true
+    field :image, String, null: true
+    field :address, String, null: true
+    field :phone, String, null: true
     field :created_at, GraphQL::Types::ISO8601DateTime, null: false
     field :updated_at, GraphQL::Types::ISO8601DateTime, null: false
   end
