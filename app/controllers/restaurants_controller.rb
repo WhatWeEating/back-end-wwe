bca03a1af7c39f1f6be0673a6efacbdab128dd4e
loganjacob76 <nagolisback@gmail.com>
2021-07-19 11:30:49 -0600

    Update controller to create event on initial query

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -4,6 +4,9 @@ class RestaurantsController < ApplicationController
 
   def index
     zip = params[:zip]
+    event_id = request.headers[:HTTP_EVENT_ID]
+
+    Event.create(uid: event_id)
 
     restaurants = RestaurantFacade.open_restaurants(zip)
 
