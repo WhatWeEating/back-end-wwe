82e41bfbe8c1b4ee54d76cd52fc3a9dc4d32e5b0
Diana Buffone <diana.pamelaet@gmail.com>
2021-07-21 00:52:37 -0400

    Tests header retrieval for event creation

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -4,9 +4,9 @@ class RestaurantsController < ApplicationController
 
   def index
     zip = params[:zip]
-    # event_id = request.headers[:HTTP_EVENT_ID]
+    event_id = request.headers[:HTTP_EVENT_ID]
     # worked for postman request TODO: remove if working as is
-    event_id = request.headers[:event_id]
+    # event_id = request.headers[:event_id]
     Event.create(uid: event_id)
 
     restaurants = RestaurantFacade.open_restaurants(zip)
