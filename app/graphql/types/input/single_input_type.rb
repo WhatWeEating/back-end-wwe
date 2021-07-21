23ad483a65a67713f575612a93ef2d57b88b0765
loganjacob76 <nagolisback@gmail.com>
2021-07-21 11:58:27 -0600

    Update requirements to vote

--- app/graphql/types/input/single_input_type.rb
+++ app/graphql/types/input/single_input_type.rb
@@ -1,11 +1,11 @@
 module Types
   module Input
     class SingleInputType < Types::BaseInputObject
-      argument :event_id, String, required: false
+      argument :event_id, String, required: true
       argument :yelp_id, String, required: true
-      argument :image, String, required: true
-      argument :address, String, required: true
-      argument :phone, String, required: true
+      argument :image, String, required: false
+      argument :address, String, required: false
+      argument :phone, String, required: false
       argument :name, String, required: false
     end
   end
