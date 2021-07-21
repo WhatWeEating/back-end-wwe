9c5a361fe2731e16446a9dcbd78c741ce840c51e
Diana Buffone <diana.pamelaet@gmail.com>
2021-07-21 14:44:36 -0400

    Allow connection for service test

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -75,4 +75,5 @@ VCR.configure do |config|
   config.hook_into :webmock
   config.filter_sensitive_data('FOOD_KEY') { ENV['FOOD_KEY'] }
   config.configure_rspec_metadata!
+  config.allow_http_connections_when_no_cassette = true
 end
