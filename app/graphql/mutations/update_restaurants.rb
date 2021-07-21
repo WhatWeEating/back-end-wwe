39be975abeac22cf30b2ab06bfdd4eff09b12938
Diana Buffone <diana.pamelaet@gmail.com>
2021-07-21 00:10:28 -0400

    Update votes with update restaurants mutation

---
+++ app/graphql/mutations/update_restaurants.rb
@@ -0,0 +1,27 @@
+module Mutations
+  class UpdateRestaurants < Mutations::BaseMutation
+    argument :params, Types::Input::RestaurantInputType, required: true
+
+    field :restaurant, Types::RestaurantType, null: true
+    field :errors, [String], null: true
+
+    def resolve(params:)
+      restaurant_params = Hash(params)
+      event = Event.find_by(uid: restaurant_params[:first][:event_id])
+
+      first = Restaurant.where(event: event, yelp_id: restaurant_params[:first][:yelp_id]).first
+      second = Restaurant.where(event: event, yelp_id: restaurant_params[:second][:yelp_id]).first
+      third = Restaurant.where(event: event, yelp_id: restaurant_params[:third][:yelp_id]).first
+
+      first.votes = first.votes +=3
+      second.votes = second.votes +=2
+      third.votes = third.votes +=1
+
+      first.save
+      second.save
+      third.save
+
+      restaurant_params[:first]
+    end
+  end
+end
--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -1,5 +1,6 @@
 module Types
   class MutationType < Types::BaseObject
     field :create_restaurants, mutation: Mutations::CreateRestaurants
+    field :update_restaurants, mutation: Mutations::UpdateRestaurants
   end
 end
